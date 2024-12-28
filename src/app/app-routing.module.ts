e1eedb9115f6fb47afea023a50cb2cc6fa4dc8ad
rohithrjnr <rohithrjnr43@gmail.com>
2024-12-28 15:55:35 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+import { FirstPageComponent } from './first-page/first-page.component';
+const routes: Routes = [
+  { path: '', component: FirstPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
