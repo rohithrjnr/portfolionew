b988baa6c1b1c0efdb4ea27f485bdec01df53342
rohithrjnr <rohithrjnr43@gmail.com>
2024-12-29 02:28:35 +0530

    Latest changes

--- src/app/projects-section/projects-section.component.ts
+++ src/app/projects-section/projects-section.component.ts
@@ -9,15 +9,22 @@ export class ProjectsSectionComponent {
   // Variables to store repository links for the popup
   frontendRepo: string = '';
   backendRepo: string = '';
-  isPopupVisible: boolean = false; // Tracks whether the popup is visible or not
+  isPopupVisible: boolean = false;
+  isOnlyoneGit: boolean = false;
 
   // Function to open the popup and set repository links
   openSourceCodePopup(frontendRepo: string, backendRepo: string): void {
     this.frontendRepo = frontendRepo;
     this.backendRepo = backendRepo;
-    this.isPopupVisible = true; // Show the popup
+  
+    if (!backendRepo) { // If only the frontend repository exists
+      this.isOnlyoneGit = true;
+      window.open(frontendRepo, '_blank'); // Open the frontend repo in a new tab
+    } else {
+      this.isOnlyoneGit = false;
+      this.isPopupVisible = true; // Show the popup to select between frontend and backend repos
+    }
   }
-
   // Function to close the popup
   closeSourceCodePopup(): void {
     this.isPopupVisible = false; // Hide the popup
